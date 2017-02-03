c0952b3e251d306d9027c68df9b4e40c6c55e297
Claire Rutkoske <claire.rutkoske@gmail.com>
2017-02-03 12:26:49 -0600

    Refactored. Log entry is now a struct. read_line is now in a reader() function.

--- src/main.rs
+++ src/main.rs
@@ -9,13 +9,26 @@ use std::io;
 use std::io::prelude::*;
 use std::string::String;
 
+
+// Log structure
+struct Single {
+  timestamp: i64,
+  entry: String,
+  tags: String,
+}
+
 // Timestamp
 fn timestamp() -> i64 {
   let timespec = time::get_time();
-    
-  let mills: i64 = (timespec.sec as i64 * 1000)+ (timespec.nsec as i64 / 1000 / 1000);
-    
+  let mills: i64 = (timespec.sec as i64 * 1000)+ (timespec.nsec as i64 / 1000 / 1000);    
   mills
+}
+
+// Read line and store
+fn reader() -> String {
+  let mut s = String::new();
+  io::stdin().read_line(&mut s).expect("Failed to read.");
+  s
 }
 
 
@@ -24,14 +37,16 @@ fn main() {
   // Create a directory, returns `io::Result<()>`
   match fs::create_dir("log") {
     Err(why) => println!("! {:?}", why.kind()),
-    Ok(_) => {},
+    Ok(_) => println!("Log dir/ created."),
   }
     
   // Create a file, returns `io::Reults<()>`
   match OpenOptions::new().create(true).write(true).open("log/log") {
     Err(why) => println!("! {:?}", why.kind()), 
-    Ok(_) => {},
+    Ok(_) => println!("Log is open."),
   }
+  
+  
     
   loop {
       
@@ -39,24 +54,27 @@ fn main() {
     
     // The log entry
     println!("Log entry:");
-    let mut entry = String::new();
-    io::stdin().read_line(&mut entry).expect("Failed to read.");
-    let entry = entry.trim();
+    let entry = reader();
       
     // Tags
     println!("Tag the entry (comma-separated):");
-    let mut tags = String::new();
-    io::stdin().read_line(&mut tags).expect("Failed to read.");
-    let tags = tags.trim();
+    let tags = reader();
+    
+    // Create Entry struct
+    let single = Single  {
+      timestamp: timestamp(),
+      entry: entry.trim().to_string(),
+      tags: tags.trim().to_string(),
+    };
       
-    writeln!(file, "{:?}, {:?}, {:?}", timestamp(), entry, tags).unwrap();
+    // Write to the log file  
+    writeln!(file, "{:?}, {:?}, {:?}", single.timestamp, single.entry, single.tags).unwrap();
       
     println!("Success!");
       
     // Add another
     println!("Type `1` to add another, or `0` to exit.");
-    let mut cont = String::new();
-    io::stdin().read_line(&mut cont).expect("Failed to read.");
+    let cont = reader();
       
     let cont: u32 = match cont.trim().parse() {
       Ok(num) => num,
@@ -68,9 +86,7 @@ fn main() {
       0 => break,
       _ => println!("Let's continue."),
     }
-    // 
       
   }
-    
     
 }
